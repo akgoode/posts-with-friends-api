952b8d0935ce6fba2e81ab971664fe58ddb8fb7f
akgoode <akgoode@gmail.com>
2017-03-27 16:35:28 -0400

    add editable property to posts

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content, :user_id
+  attributes :id, :title, :content, :editable
+
+  def editable
+    scope == object.user
+  end
 end
